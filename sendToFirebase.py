73fbc00648e14605eb91ccbec96f4d209bc9bd86
Nicolas <nicolas182@icloud.com>
2019-10-26 16:53:50 -0400

    sendToFirebase.py signals database once picture is send to storage

--- sendToFirebase.py
+++ sendToFirebase.py
@@ -1,8 +1,8 @@
-import json
 import datetime as dt
 from email.mime.image import MIMEImage
 import os
 import firebase_admin
+from firebase import firebase
 from firebase_admin import credentials
 from firebase_admin import storage
 from google.cloud import firestore
@@ -18,7 +18,10 @@ bucket = storage.bucket()
 
 def upload():
     print("Sending Image to Firestore...")
-    blob = bucket.blob( str(dt.datetime.now()) + '.jpg')
+    blob = bucket.blob( 'images/'+str(dt.datetime.now()) + '.jpg')
     outfile='/home/pi/Desktop/CameraSoftware/WhoDat.jpg'
     blob.upload_from_filename(outfile)
     print("Image was uploaded to firestore!")
+    fireapp = firebase.FirebaseApplication('https://mspi-a4b75.firebaseio.com',  None)
+    result = fireapp.post('/signal', {'signal':'Charrrrrlie'})
+    print('DB has been signaled...')
